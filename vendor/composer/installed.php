5cd62c870ec0065e36a15ff6c950c9d173ab62ae
Kotko Vladyslav <m@kotko.me>
2021-02-22 07:31:24 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '52de6f5da60ec3323d08aa8fbcfaf45ab0993b94',
+    'name' => 'pear-pear.horde.org/horde_date',
+  ),
+  'versions' => 
+  array (
+    'pear-pear.horde.org/horde_date' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52de6f5da60ec3323d08aa8fbcfaf45ab0993b94',
+    ),
+  ),
+);
